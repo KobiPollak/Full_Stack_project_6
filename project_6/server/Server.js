2b3b4330526149dd5b14e5f8ceff588f1e1749cb
Kobi Pollak <kobipollak@gmail.com>
2023-06-12 09:55:46 +0300

    changing directories of server and client side

--- project_6/server/Server.js
+++ project_6/server/Server.js
@@ -30,20 +30,47 @@ var con = mysql.createConnection({
                 console.log(JSON.parse(p));
                 console.log(results[0].id);
                 res.statusCode = 200;
-                res.send(results.length === 0 ? false : results[0].userName);
+                res.send(results.length === 0 ? JSON.stringify(false) : JSON.stringify(results[0]));
             });
           });
   })
 
-  app.get(`/info`, (req, res) => {
-    // if (err) throw err;
-    res.send('hhhhhhhhhhhhh');
-    // const sql = ``
+  app.get(`/todos/:userId`, (req, res) => {
+    const userId = req.params.userId;
+    const completedSort = req.query.completedSort || false;
+    const idSort = req.query.idSort || false;
+    const randomSort = req.query.randomSort || false;
+    con.connect(function(err){
+        if (err) throw err;
+        const sql = `select * from todos as t where t.userId = ${userId} order by ${idSort ? "id": completedSort ? "completed" : randomSort ? "RAND()" : "id"} ASC`;
+        con.query(sql, function (err, results, fields){
+            if (err) throw err;
+            console.log(results);
+            res.send(JSON.stringify(results));
+        })
+    })
+  })
+
+  app.get(`/posts/:userId`, (req, res) => {
+    const userId = req.params.userId;
+    const withComments = req.query.withComments ;
+    console.log(withComments);
+    let sql;
+    if (withComments === true){
+        sql = `select * from posts as p join comments as c on p.userId = ${userId} and p.id = c.postId`;
+    } else {
+        sql = `select * from posts where userId = ${userId}`;
+    }
+    con.query(sql, function (err, results, fields){
+        if (err) throw err;
+        console.log(results);
+        res.send(JSON.stringify(results));
+    })
   })
 
 
 
-const port = 3062; // or any port number you prefer
+const port = 3070; // or any port number you prefer
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
