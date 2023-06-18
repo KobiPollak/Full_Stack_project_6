1990e0807b81e6b185bd7ed84dc7d52ce38a422d
Kobi Pollak <kobipollak@gmail.com>
2023-06-18 09:41:49 +0300

    realization of register and info requests

--- project_6/server/Server.js
+++ project_6/server/Server.js
@@ -1,10 +1,12 @@
 var mysql = require('mysql2');
 var express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 
 var con = mysql.createConnection({
@@ -18,7 +20,7 @@ var con = mysql.createConnection({
     res.send('Hello world');
   })
 
-  app.get(`/login/:userName/:password`, (req, res) => {
+  app.get('/login/:userName/:password', (req, res) => {
     const userName = req.params.userName;
     const password = req.params.password;
     con.connect(function(err) {
@@ -39,14 +41,17 @@ var con = mysql.createConnection({
           });
   })
 
-  app.get(`/todos/:userId`, (req, res) => {
-    const userId = req.params.userId;
+  app.get(`/todos/:user_id`, (req, res) => {
+    const userId = req.params.user_id;
+    console.log(userId);
+    // const userId = 1;
     const completedSort = req.query.completedSort || false;
     const idSort = req.query.idSort || false;
     const randomSort = req.query.randomSort || false;
     con.connect(function(err){
         if (err) throw err;
-        const sql = `select * from todos as t where t.userId = ${userId} order by ${idSort ? "id": completedSort ? "completed" : randomSort ? "RAND()" : "id"} ASC`;
+        const sql = `select * from todos as t where t.userId = '${userId}' order by '${idSort ? "id": completedSort ? "completed" : randomSort ? "RAND()" : "id"}' ASC`;
+        console.log(sql);
         con.query(sql, function (err, results, fields){
             if (err) throw err;
             console.log(results);
@@ -72,24 +77,46 @@ var con = mysql.createConnection({
     })
   })
 
-  app.post('register', (req, res) => {
-    const { userName, password, name,  phone, email, address, website, company} = req.body;
-    const checkUsernameQuery = `SELECT * FROM users WHERE userName=${userName}`;
+  app.get('/info/:username', (req, res) => {
+    const username = req.params.username;
+    console.log(username);
+    const sql = `SELECT * FROM users WHERE userName = '${username}'`;
+    con.query(sql,  function (err, results, fields){
+      if (err) throw err;
+      console.log(results);
+      res.send(JSON.stringify(results[0]));
+    })
+  })
+
+  app.post('/register', (req, res) => {
+    const { username, password, name,  phone, email, address, website, company} = req.body;
+    const checkUsernameQuery = `SELECT * FROM users WHERE userName = '${username}'`;
     con.query(checkUsernameQuery, function(err, results, fields){
       if (err) {
         console.log(err);
-        res.status(500).json({ error: 'Internal server error' });
+        res.status(500).json({ error: 'Internal server error111111' });
       } else if (results.length > 0) {
         res.status(400).json({ error: 'Username already exists' });
       } else { 
-        const insertUserQuery = 'INSERT INTO users (userName, password, name,  phone, email, address, website, company) VALUES ?';
-        const values = [ userName, password, name,  phone, email, address, website, company];
+        const insertUserQuery = 'INSERT INTO users SET ?';
+        const values = { userName: username, name, phone, email, address, website, company };
+        console.log(values)
         con.query(insertUserQuery, values, (err, result) => {
           if (err){
             console.log(err);
-            result.status(500).json({ error: 'Internal server error' });
+            res.status(500).json({ error: 'Internal server error22222222' });
         } else {
-          result.json({ message: 'User registered successfully' });
+          console.log(result)
+          const insertPasswordQuery = `INSERT INTO passwords (userId, password) VALUES ('${result.insertId}', '${password}')`;
+          con.query(insertPasswordQuery, (err, result) => {
+            if (err){
+              console.log(err);
+              res.status(500).json({ error: 'Internal server error33333' });
+          } else {
+            console.log(result);
+            res.status(200).json(values)
+          }
+          })
         }
         })
       }
