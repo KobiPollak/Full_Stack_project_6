0c347f86206e0688ef2cde641c50d79537a44326
Kobi Pollak <kobipollak@gmail.com>
2023-06-18 09:39:55 +0300

    small adjustments

--- project_6/client/src/App.js
+++ project_6/client/src/App.js
@@ -7,6 +7,9 @@ import React, {useEffect} from 'react';
 import NotFound from './components/notfound';
 import Login from './components/login';
 import Logup from './components/register';
+import Application from './components/application';
+import Info from './components/info';
+import Todos from './components/todos';
 
 function App() {
   return (
@@ -14,14 +17,14 @@ function App() {
     <Routes>
       <Route exact path='/login' element={<Login />}/>
       <Route exact path='/register' element={<Logup />}/>
-      {/* <Route path='/login' element={<Login />} /> */}
-      {/* <Route path='/application/:id' element={<Application />} >
+      <Route path='/login' element={<Login />} /> 
+      <Route path='/application/:id' element={<Application />} >
         <Route index path="/application/:id/info" element={<Info />} />
         <Route path="/application/:id/todos" element={<Todos />} />
-        <Route path="/application/:id/posts" element={<Posts />} />
+        {/* <Route path="/application/:id/posts" element={<Posts />} />
         <Route path="/application/:id/albums" element={<Albums />} />
         <Route path="/application/:id/albums/:id/photos" element={<Photos />} /> */}
-      {/* </Route> */}
+      </Route>
       <Route path='*' element={<NotFound />} />
               
 
