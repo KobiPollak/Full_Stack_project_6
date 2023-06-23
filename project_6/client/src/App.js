dd4b52849f3623231c97e4608bcd21052e4f68a1
Kobi Pollak <kobipollak@gmail.com>
2023-06-23 12:56:55 +0300

    implement navigate to register page by default

--- project_6/client/src/App.js
+++ project_6/client/src/App.js
@@ -1,36 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import {BrowserRouter, Routes, Route, redirect as Redirect} from 'react-router-dom';
-import React, {useEffect} from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  redirect as Redirect,
+  Navigate,
+} from "react-router-dom";
+import React, { useEffect } from "react";
 
-import NotFound from './components/notfound';
-import Login from './components/login';
-import Logup from './components/register';
-import Application from './components/application';
-import Info from './components/info';
-import Todos from './components/todos';
-import Posts from './components/posts';
+import NotFound from "./components/notfound";
+import Login from "./components/login";
+import Logup from "./components/register";
+import Application from "./components/application";
+import Info from "./components/info";
+import Todos from "./components/todos";
+import Posts from "./components/posts";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route exact path='/login' element={<Login />}/>
-      <Route exact path='/register' element={<Logup />}/>
-      <Route path='/login' element={<Login />} /> 
-      <Route path='/application/:id' element={<Application />} >
-        <Route index path="/application/:id/info" element={<Info />} />
-        <Route path="/application/:id/todos" element={<Todos />} />
-        <Route path="/application/:id/posts" element={<Posts />} />
-        {/* <Route path="/application/:id/albums" element={<Albums />} />
+      <Routes>
+        <Route path="/" element={<Navigate to="/register" replace />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Logup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/application/:id" element={<Application />}>
+          <Route index path="/application/:id/info" element={<Info />} />
+          <Route path="/application/:id/todos" element={<Todos />} />
+          <Route path="/application/:id/posts" element={<Posts />} />
+          {/* <Route path="/application/:id/albums" element={<Albums />} />
         <Route path="/application/:id/albums/:id/photos" element={<Photos />} /> */}
-      </Route>
-      <Route path='*' element={<NotFound />} />
-              
-
-    </Routes>
-    
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </BrowserRouter>
   );
 }
